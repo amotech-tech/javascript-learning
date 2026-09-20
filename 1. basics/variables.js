4b9f97e20ea186894be1cd97ac70677a3313e6d3
Amos <amongash489@gmail.com>
2026-09-20 12:35:31 +0300

    variables in js

---
+++ 1. basics/variables.js
@@ -0,0 +1,14 @@
+let name = "Amo";
+let age = 20;
+let course = "bsc software engineering";
+
+console.log(name);
+console.log(age);
+console.log(course);
+
+
+//some calculations
+let price = 50;
+let quantity= 2;
+let total=price * quantity;
+console.log(total);
