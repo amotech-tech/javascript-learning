feed3cb2e94fb4f09423bb6bc462939f340ceae7
Amos <amongash489@gmail.com>
2026-09-21 10:26:25 +0300

    Loops practice

---
+++ 1. basics/Loops.js
@@ -0,0 +1,39 @@
+//for l00p
+for(var i=0; i<5; i++){
+    console.log(i);
+}
+ 
+//while
+let s=1;
+while(i<5){
+    console.log(s);
+    i++;
+
+}
+
+//do-while loop
+let x=1;
+do{
+    console.log(x);
+     x++;
+
+}
+while(x<5);
+
+//break
+for(let y=1; y<=10; y++){
+    if(y==5){
+        break;//loop stops
+    }
+    console.log(y);
+
+}
+
+//continue
+for(let j=1; j<=10; j++){
+    if(j==5){
+        continue;//loop do not stop
+    }
+    console.log(j);
+
+}
