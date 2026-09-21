f8314c28280072011b01de702d5a4d1aed9f906f
Amos <amongash489@gmail.com>
2026-09-22 00:15:58 +0300

    asynchronous callbacks

--- 1. basics/callbacks.js
+++ 1. basics/callbacks.js
@@ -7,3 +7,10 @@ function finish(){
     console.log("it is finished");
 }
 greet("Amo",finish);
+//
+
+console.log("start");
+setTimeout(()=>{
+    console.log("Data received");
+},2000);
+console.log("end");
