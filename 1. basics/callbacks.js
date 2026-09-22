c44efb4163876f34366ea4a080bfbbb783dbbbcb
Amos <amongash489@gmail.com>
2026-09-22 10:45:20 +0300

    callbacks

--- 1. basics/callbacks.js
+++ 1. basics/callbacks.js
@@ -14,3 +14,24 @@ setTimeout(()=>{
     console.log("Data received");
 },2000);
 console.log("end");
+
+
+function task2(callback){
+    setTimeout(() => {
+        console.log("task 2");
+        callback();
+        
+    }, 4000);
+}
+function task1(callback){
+    setTimeout(() => {
+        console.log("task 1");
+        callback();
+        
+    }, 3000);
+}
+task2(()=>{
+    task1(()=>{
+        
+    })
+})
